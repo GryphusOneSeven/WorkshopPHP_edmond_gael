3f9510ca989acafe8fee427e030dd6135909d5f0
GryphusOneSeven <gael.edmond@epitech.eu>
2023-06-22 09:38:44 +0400

    first step

---
+++ MyApp/templates/Home/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hmm yes Cake</title>
+    </head>
+    <body>
+
+    <h1>HELLO WORLD IN BIG TEXT</h1>
+
+    <?php
+    echo "Bonjour, je suis un script PHP !";
+    ?>
+
+    </body>
+</html>
